f19a8326d216f961eff5e3f288f05442e3f25bf0
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-02 18:22:39 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,5 @@
 class Hash
   def keys_of(arguments)
-    animals = {
-  "sugar glider"        => "Australia",
-  "aye-aye"             => "Madagascar",
-  "red-footed tortoise" => "Panama",
-  "kangaroo"            => "Australia",
-  "tomato frog"         => "Madagascar",
-  "koala"               => "Australia"
-    }
+
   end
 end
